5680ec9e6a7cfb2cc5dfea27b4c1d7096caf0abb
Randall <randall.gaffner.t1w9@statefarm.com>
2021-12-15 15:03:10 -0600

    added a route table

--- main.tf
+++ main.tf
@@ -38,4 +38,15 @@ resource "aws_internet_gateway" "our_iGateway" {
     }
 }
 
+# Create a Route Table
+resource "aws_route_table" "our_route_table" {
+  vpc_id = aws_vpc.SR_VPC.id
+  route {
+      cidr_block = "10.0.1.0/24"
+      gateway_id = aws_internet_gateway.our_iGateway.id
+  }
+  tags = {
+      Name = "our_route_table"
+  }  
+}
 
